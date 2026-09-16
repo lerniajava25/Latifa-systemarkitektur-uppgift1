baaa95bed4a566c848a3a551db2b22ac8d71033c
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-09-16 14:21:39 +0200

    Add build method and Builder usage in Main

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -25,41 +25,48 @@ public class Product {
         private LocalDate dateCreated;
         private LocalDate dateUpdated;
 
+        public Product build() {
+            return new Product(this);
+        }
+
         // Builder setter methods
         public Builder id(String id) {
             this.id = id;
             return this;
         }
+
         public Builder name(String name) {
             this.name = name;
             return this;
         }
+
         public Builder category(Category category) {
             this.category = category;
             return this;
         }
+
         public Builder rating(int rating) {
             this.rating = rating;
             return this;
         }
+
         public Builder dateCreated(LocalDate dateCreated) {
             this.dateCreated = dateCreated;
             return this;
         }
+
         public Builder dateUpdated(LocalDate dateUpdated) {
             this.dateUpdated = dateUpdated;
             return this;
         }
-
     }
-
-    //Origin produkt Konstruktorn -> private
-    private Product(Builder builder) {
-        this.id = builder.id;
-        this.name = builder.name;
-        this.category = builder.category;
-        this.rating = builder.rating;
-        this.dateCreated = builder.dateCreated;
-        this.dateUpdated = builder.dateUpdated;
+        //Origin produkt Konstruktorn -> private
+        private Product(Builder builder) {
+            this.id = builder.id;
+            this.name = builder.name;
+            this.category = builder.category;
+            this.rating = builder.rating;
+            this.dateCreated = builder.dateCreated;
+            this.dateUpdated = builder.dateUpdated;
+        }
     }
-}
